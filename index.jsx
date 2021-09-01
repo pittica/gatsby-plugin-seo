63b8e46822763dde9c16754a8dd19459919b4e70
Lucio Benini <iiriya@iiriya.com>
2021-09-01 19:45:18 +0200

    Fixies

---
+++ index.jsx
@@ -0,0 +1,7 @@
+import OpenGraph from "./src/open-graph"
+import TwitterCard from "./src/twitter-card"
+import SchemaOrg from "./src/schema-org"
+import Seo from "./src/seo"
+import SocialFollow from "./src/social-follow"
+
+export { Seo, SchemaOrg, OpenGraph, TwitterCard, SocialFollow }
--- src/index.jsx
+++
@@ -1,7 +0,0 @@
-import OpenGraph from "./components/open-graph"
-import TwitterCard from "./components/twitter-card"
-import SchemaOrg from "./components/schema-org"
-import Seo from "./components/seo"
-import SocialFollow from "./components/social-follow"
-
-export { Seo, SchemaOrg, OpenGraph, TwitterCard, SocialFollow }
