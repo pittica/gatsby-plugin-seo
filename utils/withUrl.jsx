7850cb56e3435e6092d2225aa2d8d5ce2dae7ee6
Lucio Benini <iiriya@iiriya.com>
2022-02-23 23:01:59 +0100

    Fix: next/prev URL

--- utils/withUrl.jsx
+++ utils/withUrl.jsx
@@ -1,5 +1,9 @@
 export default function withUrl(path, base) {
-  return /^(http:\/\/|ftp:\/\/|https:\/\/|:\/\/)[\w-]?/.test(path)
-    ? path
-    : new URL(path, base).href
+  try {
+    return /^(http:\/\/|ftp:\/\/|https:\/\/|:\/\/)[\w-]?/.test(path)
+      ? path
+      : new URL(path, base).href
+  } catch {
+    return path
+  }
 }
